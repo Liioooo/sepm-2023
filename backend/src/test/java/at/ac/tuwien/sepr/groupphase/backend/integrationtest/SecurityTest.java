bc661028cd33eaf8c2b7c640615256b527179297
Leo Preissegger <e12122526@student.tuwien.ac.at>
2023-11-08 21:58:34 +0100

    [backend] fix tests after auto generated getter/setter

--- backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/integrationtest/SecurityTest.java
+++ backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/integrationtest/SecurityTest.java
@@ -9,6 +9,10 @@ import at.ac.tuwien.sepr.groupphase.backend.entity.Message;
 import at.ac.tuwien.sepr.groupphase.backend.repository.MessageRepository;
 import at.ac.tuwien.sepr.groupphase.backend.security.JwtTokenizer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.security.DeclareRoles;
+import jakarta.annotation.security.DenyAll;
+import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
@@ -27,18 +31,21 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.annotation.security.DeclareRoles;
-import jakarta.annotation.security.DenyAll;
-import jakarta.annotation.security.PermitAll;
-import jakarta.annotation.security.RolesAllowed;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -52,156 +59,161 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 @AutoConfigureMockMvc
 public class SecurityTest implements TestData {
 
-    private static final List<Class<?>> mappingAnnotations = Lists.list(
-        RequestMapping.class,
-        GetMapping.class,
-        PostMapping.class,
-        PutMapping.class,
-        PatchMapping.class,
-        DeleteMapping.class
-    );
+  private static final List<Class<?>> mappingAnnotations = Lists.list(
+      RequestMapping.class,
+      GetMapping.class,
+      PostMapping.class,
+      PutMapping.class,
+      PatchMapping.class,
+      DeleteMapping.class
+  );
 
-    private static final List<Class<?>> securityAnnotations = Lists.list(
-        Secured.class,
-        PreAuthorize.class,
-        RolesAllowed.class,
-        PermitAll.class,
-        DenyAll.class,
-        DeclareRoles.class
-    );
+  private static final List<Class<?>> securityAnnotations = Lists.list(
+      Secured.class,
+      PreAuthorize.class,
+      RolesAllowed.class,
+      PermitAll.class,
+      DenyAll.class,
+      DeclareRoles.class
+  );
 
-    @Autowired
-    private MockMvc mockMvc;
+  @Autowired
+  private MockMvc mockMvc;
 
-    @Autowired
-    private MessageRepository messageRepository;
+  @Autowired
+  private MessageRepository messageRepository;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+  @Autowired
+  private ObjectMapper objectMapper;
 
-    @Autowired
-    private MessageMapper messageMapper;
+  @Autowired
+  private MessageMapper messageMapper;
 
-    @Autowired
-    private JwtTokenizer jwtTokenizer;
+  @Autowired
+  private JwtTokenizer jwtTokenizer;
 
-    @Autowired
-    private SecurityProperties securityProperties;
+  @Autowired
+  private SecurityProperties securityProperties;
 
-    @Autowired
-    private List<Object> components;
+  @Autowired
+  private List<Object> components;
 
-    private Message message = Message.MessageBuilder.aMessage()
-        .withTitle(TEST_NEWS_TITLE)
-        .withSummary(TEST_NEWS_SUMMARY)
-        .withText(TEST_NEWS_TEXT)
-        .withPublishedAt(TEST_NEWS_PUBLISHED_AT)
+  private Message message = Message.builder()
+      .title(TEST_NEWS_TITLE)
+      .summary(TEST_NEWS_SUMMARY)
+      .text(TEST_NEWS_TEXT)
+      .publishedAt(TEST_NEWS_PUBLISHED_AT)
+      .build();
+
+  @BeforeEach
+  public void beforeEach() {
+    messageRepository.deleteAll();
+    message = Message.builder()
+        .title(TEST_NEWS_TITLE)
+        .summary(TEST_NEWS_SUMMARY)
+        .text(TEST_NEWS_TEXT)
+        .publishedAt(TEST_NEWS_PUBLISHED_AT)
         .build();
+  }
 
-    @BeforeEach
-    public void beforeEach() {
-        messageRepository.deleteAll();
-        message = Message.MessageBuilder.aMessage()
-            .withTitle(TEST_NEWS_TITLE)
-            .withSummary(TEST_NEWS_SUMMARY)
-            .withText(TEST_NEWS_TEXT)
-            .withPublishedAt(TEST_NEWS_PUBLISHED_AT)
-            .build();
-    }
+  /**
+   * This ensures every Rest Method is secured with Method Security.
+   * It is very easy to forget securing one method causing a security vulnerability.
+   * Feel free to remove / disable / adapt if you do not use Method Security (e.g. if you prefer Web Security to define who may perform which actions) or want to use Method Security on the service layer.
+   */
+  @Test
+  public void ensureSecurityAnnotationPresentForEveryEndpoint() {
+    List<ImmutablePair<Class<?>, Method>> notSecured = components.stream()
+        .map(AopUtils::getTargetClass) // beans may be proxies, get the target class instead
+        .filter(
+            clazz -> clazz.getCanonicalName() != null && clazz.getCanonicalName().startsWith(BackendApplication.class.getPackageName())) // limit to our package
+        .filter(clazz -> clazz.getAnnotation(RestController.class) != null) // limit to classes annotated with @RestController
+        .flatMap(clazz -> Arrays.stream(clazz.getDeclaredMethods())
+            .map(method -> new ImmutablePair<Class<?>, Method>(clazz, method))) // get all class -> method pairs
+        .filter(pair -> Arrays.stream(pair.getRight().getAnnotations()).anyMatch(
+            annotation -> mappingAnnotations.contains(annotation.annotationType()))) // keep only the pairs where the method has a "mapping annotation"
+        .filter(pair -> Arrays.stream(pair.getRight().getAnnotations()).noneMatch(annotation -> securityAnnotations.contains(
+            annotation.annotationType()))) // keep only the pairs where the method does not have a "security annotation"
+        .toList();
 
-    /**
-     * This ensures every Rest Method is secured with Method Security.
-     * It is very easy to forget securing one method causing a security vulnerability.
-     * Feel free to remove / disable / adapt if you do not use Method Security (e.g. if you prefer Web Security to define who may perform which actions) or want to use Method Security on the service layer.
-     */
-    @Test
-    public void ensureSecurityAnnotationPresentForEveryEndpoint() {
-        List<ImmutablePair<Class<?>, Method>> notSecured = components.stream()
-            .map(AopUtils::getTargetClass) // beans may be proxies, get the target class instead
-            .filter(clazz -> clazz.getCanonicalName() != null && clazz.getCanonicalName().startsWith(BackendApplication.class.getPackageName())) // limit to our package
-            .filter(clazz -> clazz.getAnnotation(RestController.class) != null) // limit to classes annotated with @RestController
-            .flatMap(clazz -> Arrays.stream(clazz.getDeclaredMethods()).map(method -> new ImmutablePair<Class<?>, Method>(clazz, method))) // get all class -> method pairs
-            .filter(pair -> Arrays.stream(pair.getRight().getAnnotations()).anyMatch(annotation -> mappingAnnotations.contains(annotation.annotationType()))) // keep only the pairs where the method has a "mapping annotation"
-            .filter(pair -> Arrays.stream(pair.getRight().getAnnotations()).noneMatch(annotation -> securityAnnotations.contains(annotation.annotationType()))) // keep only the pairs where the method does not have a "security annotation"
-            .toList();
+    assertThat(notSecured.size())
+        .as("Most rest methods should be secured. If one is really intended for public use, explicitly state that with @PermitAll. "
+            + "The following are missing: \n" +
+            notSecured.stream().map(pair -> "Class: " + pair.getLeft() + " Method: " + pair.getRight()).reduce("", (a, b) -> a + "\n" + b))
+        .isZero();
 
-        assertThat(notSecured.size())
-            .as("Most rest methods should be secured. If one is really intended for public use, explicitly state that with @PermitAll. "
-                + "The following are missing: \n" + notSecured.stream().map(pair -> "Class: " + pair.getLeft() + " Method: " + pair.getRight()).reduce("", (a, b) -> a + "\n" + b))
-            .isZero();
+  }
 
-    }
-
-    @Test
-    public void givenUserLoggedIn_whenFindAll_then200() throws Exception {
-        MvcResult mvcResult = this.mockMvc.perform(get(MESSAGE_BASE_URI)
+  @Test
+  public void givenUserLoggedIn_whenFindAll_then200() throws Exception {
+    MvcResult mvcResult = this.mockMvc.perform(get(MESSAGE_BASE_URI)
             .header(securityProperties.getAuthHeader(), jwtTokenizer.getAuthToken(DEFAULT_USER, USER_ROLES)))
-            .andDo(print())
-            .andReturn();
-        MockHttpServletResponse response = mvcResult.getResponse();
+        .andDo(print())
+        .andReturn();
+    MockHttpServletResponse response = mvcResult.getResponse();
 
-        assertAll(
-            () -> assertEquals(HttpStatus.OK.value(), response.getStatus()),
-            () -> assertEquals(MediaType.APPLICATION_JSON_VALUE, response.getContentType())
-        );
-    }
+    assertAll(
+        () -> assertEquals(HttpStatus.OK.value(), response.getStatus()),
+        () -> assertEquals(MediaType.APPLICATION_JSON_VALUE, response.getContentType())
+    );
+  }
 
-    @Test
-    public void givenNoOneLoggedIn_whenFindAll_then401() throws Exception {
-        MvcResult mvcResult = this.mockMvc.perform(get(MESSAGE_BASE_URI))
-            .andDo(print())
-            .andReturn();
-        MockHttpServletResponse response = mvcResult.getResponse();
+  @Test
+  public void givenNoOneLoggedIn_whenFindAll_then401() throws Exception {
+    MvcResult mvcResult = this.mockMvc.perform(get(MESSAGE_BASE_URI))
+        .andDo(print())
+        .andReturn();
+    MockHttpServletResponse response = mvcResult.getResponse();
 
-        assertEquals(HttpStatus.FORBIDDEN.value(), response.getStatus());
-    }
+    assertEquals(HttpStatus.FORBIDDEN.value(), response.getStatus());
+  }
 
-    @Test
-    public void givenAdminLoggedIn_whenPost_then201() throws Exception {
-        MessageInquiryDto messageInquiryDto = messageMapper.messageToMessageInquiryDto(message);
-        String body = objectMapper.writeValueAsString(messageInquiryDto);
+  @Test
+  public void givenAdminLoggedIn_whenPost_then201() throws Exception {
+    MessageInquiryDto messageInquiryDto = messageMapper.messageToMessageInquiryDto(message);
+    String body = objectMapper.writeValueAsString(messageInquiryDto);
 
-        MvcResult mvcResult = this.mockMvc.perform(post(MESSAGE_BASE_URI)
+    MvcResult mvcResult = this.mockMvc.perform(post(MESSAGE_BASE_URI)
             .contentType(MediaType.APPLICATION_JSON)
             .content(body)
             .header(securityProperties.getAuthHeader(), jwtTokenizer.getAuthToken(ADMIN_USER, ADMIN_ROLES)))
-            .andDo(print())
-            .andReturn();
-        MockHttpServletResponse response = mvcResult.getResponse();
+        .andDo(print())
+        .andReturn();
+    MockHttpServletResponse response = mvcResult.getResponse();
 
-        assertEquals(HttpStatus.CREATED.value(), response.getStatus());
-    }
+    assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+  }
 
-    @Test
-    public void givenNoOneLoggedIn_whenPost_then403() throws Exception {
-        message.setPublishedAt(null);
-        MessageInquiryDto messageInquiryDto = messageMapper.messageToMessageInquiryDto(message);
-        String body = objectMapper.writeValueAsString(messageInquiryDto);
+  @Test
+  public void givenNoOneLoggedIn_whenPost_then403() throws Exception {
+    message.setPublishedAt(null);
+    MessageInquiryDto messageInquiryDto = messageMapper.messageToMessageInquiryDto(message);
+    String body = objectMapper.writeValueAsString(messageInquiryDto);
 
-        MvcResult mvcResult = this.mockMvc.perform(post(MESSAGE_BASE_URI)
+    MvcResult mvcResult = this.mockMvc.perform(post(MESSAGE_BASE_URI)
             .contentType(MediaType.APPLICATION_JSON)
             .content(body))
-            .andDo(print())
-            .andReturn();
-        MockHttpServletResponse response = mvcResult.getResponse();
+        .andDo(print())
+        .andReturn();
+    MockHttpServletResponse response = mvcResult.getResponse();
 
-        assertEquals(HttpStatus.FORBIDDEN.value(), response.getStatus());
-    }
+    assertEquals(HttpStatus.FORBIDDEN.value(), response.getStatus());
+  }
 
-    @Test
-    public void givenUserLoggedIn_whenPost_then403() throws Exception {
-        message.setPublishedAt(null);
-        MessageInquiryDto messageInquiryDto = messageMapper.messageToMessageInquiryDto(message);
-        String body = objectMapper.writeValueAsString(messageInquiryDto);
+  @Test
+  public void givenUserLoggedIn_whenPost_then403() throws Exception {
+    message.setPublishedAt(null);
+    MessageInquiryDto messageInquiryDto = messageMapper.messageToMessageInquiryDto(message);
+    String body = objectMapper.writeValueAsString(messageInquiryDto);
 
-        MvcResult mvcResult = this.mockMvc.perform(post(MESSAGE_BASE_URI)
+    MvcResult mvcResult = this.mockMvc.perform(post(MESSAGE_BASE_URI)
             .contentType(MediaType.APPLICATION_JSON)
             .content(body)
             .header(securityProperties.getAuthHeader(), jwtTokenizer.getAuthToken(DEFAULT_USER, USER_ROLES)))
-            .andDo(print())
-            .andReturn();
-        MockHttpServletResponse response = mvcResult.getResponse();
+        .andDo(print())
+        .andReturn();
+    MockHttpServletResponse response = mvcResult.getResponse();
 
-        assertEquals(HttpStatus.FORBIDDEN.value(), response.getStatus());
-    }
+    assertEquals(HttpStatus.FORBIDDEN.value(), response.getStatus());
+  }
 }
--- backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/unittests/MessageRepositoryTest.java
+++ backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/unittests/MessageRepositoryTest.java
@@ -10,7 +10,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(SpringExtension.class)
 // This test slice annotation is used instead of @SpringBootTest to load only repository beans instead of
@@ -19,24 +21,24 @@ import static org.junit.jupiter.api.Assertions.*;
 @ActiveProfiles("test")
 public class MessageRepositoryTest implements TestData {
 
-    @Autowired
-    private MessageRepository messageRepository;
+  @Autowired
+  private MessageRepository messageRepository;
 
-    @Test
-    public void givenNothing_whenSaveMessage_thenFindListWithOneElementAndFindMessageById() {
-        Message message = Message.MessageBuilder.aMessage()
-            .withTitle(TEST_NEWS_TITLE)
-            .withSummary(TEST_NEWS_SUMMARY)
-            .withText(TEST_NEWS_TEXT)
-            .withPublishedAt(TEST_NEWS_PUBLISHED_AT)
-            .build();
+  @Test
+  public void givenNothing_whenSaveMessage_thenFindListWithOneElementAndFindMessageById() {
+    Message message = Message.builder()
+        .title(TEST_NEWS_TITLE)
+        .summary(TEST_NEWS_SUMMARY)
+        .text(TEST_NEWS_TEXT)
+        .publishedAt(TEST_NEWS_PUBLISHED_AT)
+        .build();
 
-        messageRepository.save(message);
+    messageRepository.save(message);
 
-        assertAll(
-            () -> assertEquals(1, messageRepository.findAll().size()),
-            () -> assertNotNull(messageRepository.findById(message.getId()))
-        );
-    }
+    assertAll(
+        () -> assertEquals(1, messageRepository.findAll().size()),
+        () -> assertNotNull(messageRepository.findById(message.getId()))
+    );
+  }
 
 }
