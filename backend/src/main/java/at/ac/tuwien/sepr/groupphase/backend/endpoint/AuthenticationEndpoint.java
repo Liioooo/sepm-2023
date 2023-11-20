3204b290410cc34d19fd56140e0a87d0dd1f333e
Leo Preissegger <e12122526@student.tuwien.ac.at>
2023-11-20 21:25:08 +0100

    [backend] #1, #42 add Swagger UI docs

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/AuthenticationEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/AuthenticationEndpoint.java
@@ -3,6 +3,7 @@ package at.ac.tuwien.sepr.groupphase.backend.endpoint;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.UserLoginDto;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.UserRegisterDto;
 import at.ac.tuwien.sepr.groupphase.backend.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.annotation.security.PermitAll;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,12 +23,14 @@ public class AuthenticationEndpoint {
 
     @PermitAll
     @PostMapping(path = "/login")
+    @Operation(summary = "Login as a User")
     public String login(@Valid @RequestBody UserLoginDto userLoginDto) {
         return userService.login(userLoginDto);
     }
 
     @PermitAll
     @PostMapping(path = "/register")
+    @Operation(summary = "Register as a new User")
     public String register(@Valid @RequestBody UserRegisterDto userRegisterDto) {
         return userService.register(userRegisterDto);
     }
