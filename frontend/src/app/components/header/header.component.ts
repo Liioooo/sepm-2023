b0a1f49e0cf94a0dd8ec5937ea95833f9152d4d0
Tobias Seczer <tobias.seczer@tuwien.ac.at>
2023-11-20 13:17:50 +0100

    [frontend] #39 adjust mobile navbar and add bootstrap-icons

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -7,6 +7,7 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public isNavbarCollapsed: boolean = true;
 
   constructor(public authService: AuthService) { }
 
