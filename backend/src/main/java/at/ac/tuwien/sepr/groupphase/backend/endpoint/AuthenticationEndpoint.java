ac9225ee276bb2c3bc31e1274be024e360675b33
Andreas <andreas.sch4@gmail.com>
2023-12-04 00:20:30 +0100

    [backend] #29 use unauthorizedException for reset password invalid tokens

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/AuthenticationEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/AuthenticationEndpoint.java
@@ -4,28 +4,23 @@ import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.EmailResetDto;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.ResetPasswordDto;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.UserLoginDto;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.UserRegisterDto;
+import at.ac.tuwien.sepr.groupphase.backend.exception.UnauthorizedException;
 import at.ac.tuwien.sepr.groupphase.backend.service.UserService;
 import io.jsonwebtoken.JwtException;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.annotation.security.PermitAll;
 import jakarta.validation.Valid;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.lang.invoke.MethodHandles;
 
 @RestController
 @RequestMapping(value = "/api/v1/authentication")
 public class AuthenticationEndpoint {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private final UserService userService;
 
     public AuthenticationEndpoint(UserService userService) {
@@ -62,8 +57,7 @@ public class AuthenticationEndpoint {
         try {
             userService.resetPassword(resetPasswordDto);
         } catch (JwtException e) {
-            LOGGER.error("Invalid Token provided", e);
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid Token", e);
+            throw new UnauthorizedException("Invalid Token", e);
         }
     }
 
