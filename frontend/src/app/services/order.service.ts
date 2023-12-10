7ba9814e89b3ab36024f56655f34f1d8d2966e5e
Andreas <andreas.sch4@gmail.com>
2023-12-11 00:35:46 +0100

    [frontend] #18 add functions to order service for retrieving and modifying orders and reservations

--- frontend/src/app/services/order.service.ts
+++ frontend/src/app/services/order.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable, pipe } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Globals } from '../global/globals';
 import { OrderCreateDto } from '../dtos/order-create-dto';
+import { OrderListDto } from '../dtos/order-list-dto';
+import { convertToDatesInObject } from '../utils/convertToDatesInObject';
+import { OrderDetailDto } from '../dtos/order-detail-dto';
+import { OrderType } from '../types/order-type';
 
 @Injectable({
   providedIn: 'root'
@@ -15,12 +19,49 @@ export class OrderService {
   }
 
   /**
-   * Get Events from backend, optionally filtered by search parameters
+   * Create a new order
    *
    * @param orderCreateDto Order to create
    */
   createOrder(orderCreateDto: OrderCreateDto): Observable<void> {
     return this.httpClient.post<void>(this.baseUri, orderCreateDto);
+  }
+
+  /**
+   * Get an order by its id
+   * @param orderId
+   */
+  getOrder(orderId: number): Observable<OrderDetailDto> {
+    return this.httpClient.get<OrderDetailDto>(`${this.baseUri}/${orderId}`).pipe(
+      map(convertToDatesInObject)
+    );
+  }
+
+  /**
+   * Get all orders of the current user
+   */
+  getOwnOrders(): Observable<OrderListDto[]> {
+    return this.httpClient.get<OrderListDto[]>(this.baseUri).pipe(
+      map(convertToDatesInObject)
+    );
+  }
+
+  /**
+   * Delete a reservation by its id
+   * @param orderId
+   */
+  purchaseReservation(orderId: number): Observable<void> {
+    return this.httpClient.patch<void>(`${this.baseUri}/${orderId}`, {
+      orderType: OrderType.BUY
+    });
+  }
+
+  /**
+   * Delete a reservation by its id
+   * @param orderId
+   */
+  deleteReservation(orderId: number): Observable<void> {
+    return this.httpClient.delete<void>(`${this.baseUri}/${orderId}`);
   }
 
 }
