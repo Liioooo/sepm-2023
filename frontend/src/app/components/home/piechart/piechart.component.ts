6548a20f690cea41382d2e0a45e8bb40fa382f9a
Val <valentina.kicker@hotmail.com>
2023-12-10 19:41:02 +0100

    [frontend] #10 changed colour palette of piechart

--- frontend/src/app/components/home/piechart/piechart.component.ts
+++ frontend/src/app/components/home/piechart/piechart.component.ts
@@ -1,49 +1,50 @@
-import {Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import Chart from 'chart.js/auto';
-import {plugins} from "chart.js";
 
 @Component({
   selector: 'app-piechart',
   templateUrl: './piechart.component.html',
   styleUrls: ['./piechart.component.scss']
 })
-export class PiechartComponent implements OnInit {
-  @Input() searchMonth: string = "";
-  @Input() searchCategory: string = "";
-  constructor() { }
+export class PiechartComponent implements OnInit, OnChanges {
+  @Input() searchMonth: string = '';
+  @Input() eventType: string = '';
+  @Input() chartData: number[] = []; // Input for dynamic data
+
+  constructor() {}
 
   ngOnInit(): void {
     this.createChart();
   }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.chartData && !changes.chartData.firstChange) {
+      // Update the chart when the input data changes
+      this.updateChart();
+    }
+  }
+
   public chart: any;
 
   createChart() {
-    this.chart = new Chart("MyChart", {
+    this.chart = new Chart('Top 10 Events', {
       type: 'pie',
-      // type: 'doughnut',
       data: {
-        labels: [
-          //da gehören die Event names hin
-          'Wheat',
-          'Maize',
-          'Rice',
-          'Sugarcane',
-          'Cotton'
-        ],
+        labels: [], // You can add labels dynamically if needed
         datasets: [{
-          label: 'Top 10 '+ this.searchCategory + ' in '+ this.searchMonth,
-          data: [9168.2, 1417.8, 3335.1, 1165.0, 2078.9],
+          label: 'Top 10 ' + this.eventType + ' in ' + this.searchMonth,
+          data: this.chartData,
           backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)',
-            'rgb(75, 192, 192)',
-            'rgb(153, 102, 255)',
-            'rgb(150, 12, 200)',
-            'rgb(100, 120, 100)',
-            'rgb(50, 120, 100)',
-            'rgb(0, 0, 100)',
-            'rgb(220, 20, 80)'
+            'rgb(1,21,104)',
+            'rgb(70,61,133)',
+            'rgb(117,105,163)',
+            'rgb(162,153,193)',
+            'rgb(208,203,224)',
+            'rgb(255,221,218)',
+            'rgb(255,187,182)',
+            'rgb(251,153,147)',
+            'rgb(244,117,114)',
+            'rgb(233,77,82)'
           ],
           hoverOffset: 4
         }]
@@ -53,11 +54,11 @@ export class PiechartComponent implements OnInit {
         plugins: {
           title: {
             display: true,
-            text: 'Top 10 '+ this.searchCategory + ' in '+ this.searchMonth,
+            text: 'Top 10 ' + this.eventType + ' in ' + this.searchMonth,
             font: {
               size: 24,
               weight: 'bold',
-              family: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif"
+              family: '\'Helvetica Neue\', \'Helvetica\', \'Arial\', sans-serif'
             },
             padding: {
               top: 10,
@@ -69,11 +70,18 @@ export class PiechartComponent implements OnInit {
             labels: {
               font: {
                 size: 14,
-                family: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif"
+                family: '\'Helvetica Neue\', \'Helvetica\', \'Arial\', sans-serif'
               }
             }
           }
         }
       }
     });
-  }}
+  }
+
+  updateChart() {
+    // Update the chart data when the input data changes
+    this.chart.data.datasets[0].data = this.chartData;
+    this.chart.update();
+  }
+}
