736fe7ddd0e33c6a3fab416184926b7a0f382b24
Val <valentina.kicker@hotmail.com>
2023-12-13 15:11:14 +0100

    [frontend] #10 merge with dev issues with entities

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/EventMapper.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/mapper/EventMapper.java
@@ -3,10 +3,12 @@ package at.ac.tuwien.sepr.groupphase.backend.endpoint.mapper;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.EventDetailDto;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.EventListDto;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.EventWithBoughtCountDto;
-import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.Top10EventDisplayDto;
 import at.ac.tuwien.sepr.groupphase.backend.entity.Event;
 import at.ac.tuwien.sepr.groupphase.backend.entity.interfaces.EventWithBoughtCount;
 import org.mapstruct.Mapper;
+
+import java.util.Collection;
+import java.util.List;
 
 @Mapper
 public interface EventMapper {
@@ -17,6 +19,5 @@ public interface EventMapper {
 
     List<EventWithBoughtCountDto> eventWithBoughtToEventWithBoughtDtoList(Collection<EventWithBoughtCount> events);
 
-    List<Top10EventDisplayDto> eventWithBoughtToTop10EventDisplayDto(Collection<EventWithBoughtCount> events);
 
 }
--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/EventRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/EventRepository.java
@@ -42,7 +42,7 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     Page<Event> findBySearchCriteria(@Param("search") EventSearchDto search, Pageable pageable);
 
     // TODO: WHERE t.order.type != 'CANCELLED'
-    @Query("SELECT e as event, (SELECT COUNT(t) FROM e.tickets t) AS boughtCount FROM Event e"
+    @Query("SELECT e as event, (SELECT COUNT(t) FROM e.orders. t) AS boughtCount FROM Event e"
         + " WHERE e.startDate <= (:endDate) AND e.endDate >= (:startDate) AND ((:type) IS NULL OR e.type = (:type))"
         + " ORDER BY (SELECT COUNT(t) FROM e.tickets t) DESC"
         + " LIMIT 10"
