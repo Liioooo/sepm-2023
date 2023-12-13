c1e59a4d9d9b63e38f3d7c68b7b71f165183ef5e
Leo Preissegger <e12122526@student.tuwien.ac.at>
2023-12-13 16:16:18 +0100

    [frontend] #10 findTopTenEvents respects canceled orders

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/EventRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/EventRepository.java
@@ -41,10 +41,9 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     )
     Page<Event> findBySearchCriteria(@Param("search") EventSearchDto search, Pageable pageable);
 
-    // TODO: WHERE t.order.type != 'CANCELLED'
-    @Query("SELECT e as event, (SELECT COUNT(t) FROM Ticket t WHERE t.order.event = e) AS boughtCount FROM Event e"
+    @Query("SELECT e as event, (SELECT COUNT(t) FROM Ticket t WHERE t.order.event = e AND t.order.cancellationDate IS NULL) AS boughtCount FROM Event e"
         + " WHERE e.startDate <= (:endDate) AND e.endDate >= (:startDate) AND ((:type) IS NULL OR e.type = (:type))"
-        + " ORDER BY (SELECT COUNT(t) FROM Ticket t WHERE t.order.event = e) DESC"
+        + " ORDER BY (SELECT COUNT(t) FROM Ticket t WHERE t.order.event = e AND t.order.cancellationDate IS NULL) DESC"
         + " LIMIT 10"
     )
     List<EventWithBoughtCount> findTopTenEvent(@Param("startDate") OffsetDateTime startDate, @Param("endDate") OffsetDateTime endDate, @Param("type") EventType type);
