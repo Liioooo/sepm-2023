3f03958977910d17733579ed32857bb1950e37f0
Andreas <andreas@bitberry.io>
2023-12-14 17:13:04 +0100

    [backend] fix: checkstyle errors

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/EventListDto.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/EventListDto.java
@@ -1,7 +1,13 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint.dto;
 
 import at.ac.tuwien.sepr.groupphase.backend.enums.EventType;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.time.OffsetDateTime;
 
--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/PageDto.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/PageDto.java
@@ -1,6 +1,13 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint.dto;
 
-import lombok.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
