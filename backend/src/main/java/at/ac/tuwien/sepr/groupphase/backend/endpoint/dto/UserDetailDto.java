f38222647388a7adc4e25c08f894580a77f27396
Hannah Ertl <e12119039@student.tuwien.ac.at>
2023-12-14 18:43:03 +0100

    [backend] #45 add noArgsConstructor

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/UserDetailDto.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/UserDetailDto.java
@@ -3,11 +3,13 @@ package at.ac.tuwien.sepr.groupphase.backend.endpoint.dto;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @EqualsAndHashCode
 @AllArgsConstructor
 @ToString
