adf0fc951ba29a05eda2c5d4bcdd68f7dc95bf6f
Elias Huhsovitz <elias.huhsovitz@tuwien.ac.at>
2023-12-30 03:33:22 +0100

    [backend] #45 Respected NULL values in news.author when searching for news via Criteria

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/NewsRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/NewsRepository.java
@@ -20,10 +20,29 @@ public interface NewsRepository extends JpaRepository<News, Long> {
 
     Collection<News> findAllByTitleContains(String title);
 
+    /*
+    @Query("SELECT n FROM News n WHERE "
+        + "(:#{#search.title} IS NULL OR UPPER(n.title) LIKE UPPER(CONCAT('%', :#{#search.title}, '%'))) AND "
+        + "(n.author IS NOT NULL AND ("
+        + "   (:#{#search.authorName} IS NULL) OR "
+        + "   (UPPER(n.author.firstName) LIKE UPPER(CONCAT('%', :#{#search.authorName}, '%')) OR "
+        + "    UPPER(n.author.lastName) LIKE UPPER(CONCAT('%', :#{#search.authorName}, '%')))"
+        + " )) "
+        + "ORDER BY n.publishDate DESC")
+
+
     @Query("SELECT n FROM News n WHERE "
         + "(:#{#search.title} IS NULL OR UPPER(n.title) LIKE UPPER(CONCAT('%', :#{#search.title}, '%'))) AND "
         + "(:#{#search.authorName} IS NULL OR (UPPER(n.author.firstName) LIKE UPPER(CONCAT('%', :#{#search.authorName}, '%')) OR "
         + " UPPER(n.author.lastName) LIKE UPPER(CONCAT('%', :#{#search.authorName}, '%')))) "
+        + "ORDER BY n.publishDate DESC")
+
+     */
+    @Query("SELECT n FROM News n WHERE "
+        + "(:#{#search.title} IS NULL OR UPPER(n.title) LIKE UPPER(CONCAT('%', :#{#search.title}, '%'))) AND "
+        + "(:#{#search.authorName} IS NULL OR "
+        + "(COALESCE(UPPER(n.author.firstName), '') LIKE UPPER(CONCAT('%', :#{#search.authorName}, '%')) OR "
+        + "COALESCE(UPPER(n.author.lastName), '') LIKE UPPER(CONCAT('%', :#{#search.authorName}, '%')))) "
         + "ORDER BY n.publishDate DESC")
     Page<News> findNewsBySearchCriteria(@Param("search") NewsSearchDto search, Pageable pageable);
 }
