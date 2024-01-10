2cbe74a0407c065322522db83b928f072f7279c8
Hannah Ertl <e12119039@student.tuwien.ac.at>
2024-01-10 10:34:07 +0100

    [frontend][backend] #15 fix validation

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/EventCreateDto.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/EventCreateDto.java
@@ -1,6 +1,11 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint.dto;
 
 import at.ac.tuwien.sepr.groupphase.backend.enums.EventType;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -20,21 +25,32 @@ import java.time.OffsetDateTime;
 @ToString
 public class EventCreateDto {
 
+    @NotBlank(message = "Title is required")
+    @Size(max = 255, message = "Title must not exceed 255 characters")
     private String title;
 
+    @NotNull(message = "Start Date is required")
     private OffsetDateTime startDate;
 
+    @NotNull(message = "End Date is required")
     private OffsetDateTime endDate;
 
+    @NotNull(message = "Seat Price is required")
+    @PositiveOrZero(message = "Seat Price must be greater or equal to 0")
     private Float seatPrice;
 
+    @NotNull(message = "Standing Price is required")
+    @PositiveOrZero(message = "Standing Price must be greater or equal to 0")
     private Float standingPrice;
 
+    @NotNull(message = "Hall ID is required")
     private Long hallId;
 
+    @NotNull(message = "Artist ID is required")
     private Long artistId;
 
     private MultipartFile image;
 
+    @NotNull(message = "Event Type is required")
     private EventType type;
 }
