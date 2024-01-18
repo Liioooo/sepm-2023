96425d3d159fd3c054f1cc71e9091c6c59db0c18
Val <valentina.kicker@hotmail.com>
2024-01-18 21:40:46 +0100

    [frontend] #60 reformatted code

--- frontend/src/app/components/contact-admin/contact-admin.component.ts
+++ frontend/src/app/components/contact-admin/contact-admin.component.ts
@@ -1,11 +1,4 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ControlsOf } from '../../types/controls-of';
-import { UserLoginDto } from '../../dtos/user-login-dto';
-import { UserService } from '../../services/user.service';
-import { Router } from '@angular/router';
-import { ToastService } from '../../services/toast.service';
-import { ErrorFormatterService } from '../../services/error-formatter.service';
 
 @Component({
   selector: 'app-contact-admin',
@@ -13,19 +6,4 @@ import { ErrorFormatterService } from '../../services/error-formatter.service';
   styleUrls: ['./contact-admin.component.scss']
 })
 export class ContactAdminComponent {
-
-  loginForm: FormGroup<ControlsOf<UserLoginDto>>;
-
-  constructor(
-    private formBuilder: FormBuilder
-  ) {
-    this.loginForm = this.formBuilder.group<ControlsOf<UserLoginDto>>({
-      email: this.formBuilder.control('', [Validators.required, Validators.email]),
-      password: this.formBuilder.control('', [Validators.required, Validators.minLength(8)])
-    });
-  }
-  loginUser() {
-    this.loginForm.markAllAsTouched();
-  }
-
 }
